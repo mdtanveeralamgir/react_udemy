4d18ecd6a7c5b0949fdd41d495be0eb8afff2f76
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-08 23:01:10 -0400

    adding data into elements using variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 94;
   return (
     <div className="expense-item">
-      <div>October 8th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">$94</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
