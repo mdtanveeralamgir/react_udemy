6d6603d9eab751d2f883ce8520291bf5b8806268
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-10 01:44:12 -0400

    Controlled component: pass to and receive from parent component, adding ExpenseFilter component (no implementation)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,34 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 function Expenses(props) {
-  const expenseItems = props.items.map((item) => 
+  const [year, setYear] = useState("2020");
+  let items = props.items;
+
+  const changeYear = (year) => {
+    setYear(year);
+  };
+
+  const expenseItems = items.map((item) => (
     <ExpenseItem
       key={item.id}
       title={item.title}
       amount={item.amount}
       date={item.date}
     />
+  ));
+
+  return (
+    <div>
+      <Card className="expenses">
+      <ExpenseFilter selected={year} onSelectYear={changeYear}/>
+      {expenseItems}
+      </Card>;
+    </div>
   );
-  return <Card className="expenses">{expenseItems}</Card>;
 }
 
 export default Expenses;
