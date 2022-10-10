3ad589c9412d0db4064b5ece9d5a1284f4262aea
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-09 22:53:19 -0400

    submit the form and show the input data in console

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,49 +2,61 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 function ExpenseForm() {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     //this is a bad approach since it depends on previous state of other inputs
     //So if there is any counter in the form this will not show the right value
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value, //adding all the user input and overriding only title
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value, //adding all the user input and overriding only title
+    // });
     //Better way is this one
-    //Because of the scheduling of state this below way will ensure that 
+    //Because of the scheduling of state this below way will ensure that
     //The prevState is always the current state passed as a param
-    setUserInput(() => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
+    // setUserInput(() => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
   const dateChangehandler = (event) => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    }
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
