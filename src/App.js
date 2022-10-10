655732e7c47535b8fc33af38bd618a2d03a96c0a
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-10 02:07:24 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import { useState } from "react";
 
+const expenses = [
+  { id: "e1", title: "Toilet paper", amount: 40, date: new Date(2022, 9, 8) },
+  {
+    id: "e2",
+    title: "Car Inusrance",
+    amount: 100,
+    date: new Date(2022, 9, 7),
+  },
+  { id: "e3", title: "Desk", amount: 350, date: new Date(2022, 9, 6) },
+];
+
+
 function App() {
-  const expenses = [
-    { id: "e1", title: "Toilet paper", amount: 40, date: new Date(2022, 9, 8) },
-    {
-      id: "e2",
-      title: "Car Inusrance",
-      amount: 100,
-      date: new Date(2022, 9, 7),
-    },
-    { id: "e3", title: "Desk", amount: 350, date: new Date(2022, 9, 6) },
-  ];
 
   const [expenseItems, setExpenseItems] = useState(expenses);
 
