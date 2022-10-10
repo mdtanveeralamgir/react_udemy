cec82bd7bcb613f75f579277e4ead3310eece521
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-09 20:03:01 -0400

    making expense component and adding it in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
-    { id: 'e1', title: "Toilet paper", amount: 40, date: new Date(2022, 9, 8) },
-    { id: 'e2',title: "Car Inusrance", amount: 100, date: new Date(2022, 9, 7) },
-    { id: 'e3', title: "Desk", amount: 350, date: new Date(2022, 9, 6) },
+    { id: "e1", title: "Toilet paper", amount: 40, date: new Date(2022, 9, 8) },
+    {
+      id: "e2",
+      title: "Car Inusrance",
+      amount: 100,
+      date: new Date(2022, 9, 7),
+    },
+    { id: "e3", title: "Desk", amount: 350, date: new Date(2022, 9, 6) },
   ];
   return (
     <div>
-      <h1>Let's get started</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <Expenses
+        items = {expenses}
+      />
     </div>
   );
 }
