6293645733cfb0b19755c7798f6c7271e6d677c9
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-12 23:13:53 -0400

    adding multiple and conditional css classes using css module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,38 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-import styled from "styled-components";
+import styles from "./CourseInput.module.css";
+// import styled from "styled-components";
 
-const FormControl = styled.div`
-   {
-    margin: 0.5rem 0;
-  }
+// const FormControl = styled.div`
+//    {
+//     margin: 0.5rem 0;
+//   }
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")}
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")}
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    ${"" /* changing color based on the value passed as props */}
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#ffd7d" : "transparent")}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     ${"" /* changing color based on the value passed as props */}
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#ffd7d" : "transparent")}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -60,10 +60,17 @@ const CourseInput = (props) => {
       {/* Adding only invalid class since all other class are adding in the FormControl */}
       {/* <FormControl className={!isValid && 'invalid'}> */}
       {/* Adding props to styled component and utilize in the FormControl class */}
-      <FormControl invalid={!isValid}>
+      {/* <FormControl invalid={!isValid}> */}
+      {/* adding style using react class module 
+        - method nane cannot have '-'
+        - so below is the way to overcome that limitation
+        - adding multiple classes using template literal
+      */}
+
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
