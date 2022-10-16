4b47224120563febc08afdb4a7cfb10d4233116c
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-16 00:52:58 -0400

    Handling form verification using useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //It will run after all the rendering is done and if the dependencies changed
+  //if the dependencies array is empty then it's called onMount: only run the useEffect call back function when the 
+  // component starts
+  useEffect(() => {
+    const loggingStatus = localStorage.getItem('isLoggedIn');
+
+    if(loggingStatus === '1')
+    {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
     setIsLoggedIn(false);
   };
 
