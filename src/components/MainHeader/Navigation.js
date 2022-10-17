2c50947f74146e02f86d41c23ef14cfdef878880
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-17 12:52:58 -0400

    using useContext hook to get the value of AuthContext provider

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,38 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 import AuthContext from "../Context/auth-context";
 
 const Navigation = (props) => {
+
+  //Using context hook to get the value of isLoggedin from AuthContext provider
+  const ctx = useContext(AuthContext);
+  
   return (
-    <AuthContext.Consumer>
-      {/* This ctx param is passing the object available in authcontext
-       * so in order to use ctx we need to put all jsx code inside that function
-       */}
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
