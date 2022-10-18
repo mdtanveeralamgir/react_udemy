e50c4f98c70dbcb2c612dd4c2ef22b5426f3200e
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-18 10:39:21 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { AuthContextprovider } from "./components/Context/auth-context";
 
 import "./index.css";
 import App from "./App";
-import { useContext } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
