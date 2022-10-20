1ad59269cc20eb250f63f67880e3fd950f4d4858
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-20 11:41:06 -0400

    creating a separate file for cart context and adding totalAmount on the cart using context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,18 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{ctx.totalAmounts}</span>
     </button>
   );
 };
