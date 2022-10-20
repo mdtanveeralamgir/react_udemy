f5e8313f1e6a03ab3bb1fd38ac90e0d84eb45bfe
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-20 11:27:46 -0400

    All the front end and cart show hide logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarthandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCarthandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div>
-      <h1>Let's get started</h1>
-    </div>
+    <Fragment>
+      {cartIsShown && <Cart onCloseCart={hideCarthandler}/>}
+      <Header onShowCart={showCarthandler}/>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
