46f28ce8e19631c1aae3756d72197da08ffd4f57
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-20 11:47:40 -0400

    refactoring cart items number to reflect total items added to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,13 +6,21 @@ import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
 
+  //Reduce an array to a single number
+  //Initially the curNumber is 0 as passed to the second argu
+  // Then each item's amount will be added to the curNumber
+  const numberOfCartItems = ctx.items.reduce((curNumber, item)=>{
+    return curNumber + item.amount;
+  }, 0);
+
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{ctx.totalAmounts}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
