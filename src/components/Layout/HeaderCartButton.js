af354e86dd353e11e1bad3baf4165573ca2e1594
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-20 23:45:55 -0400

    adding bump effect to the cart button using useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,39 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setButtonIsHighlighted] = useState(false);
   const ctx = useContext(CartContext);
-
+  const {items} = ctx;
   //Reduce an array to a single number
   //Initially the curNumber is 0 as passed to the second argu
   // Then each item's amount will be added to the curNumber
-  const numberOfCartItems = ctx.items.reduce((curNumber, item)=>{
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
 
+  
+  useEffect(() => {
+    if(ctx.items.length ===0){
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return()=>{
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
