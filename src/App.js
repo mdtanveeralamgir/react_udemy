f7d09cf67cb50386e1ae6c4c07a1322ea76f94ff
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-24 20:41:30 -0400

    instead of adding a new paragraph on button click adding the text inside paragraph

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 /*
-* React loads only the elements that has changed rather than reloading each elements a component has
-* In the below example the console.log will re-run every time the toggle button is clicked
-* Because the whole component re-loads
-* But only the <p> will be added/removed since from previous state to current state only <p> is changing
-*/
+ * React loads only the elements that has changed rather than reloading each elements a component has
+ * In the below example the console.log will re-run every time the toggle button is clicked
+ * Because the whole component re-loads
+ * But only the <p> will be added/removed since from previous state to current state only <p> is changing
+ */
 function App() {
   const [showP, setShowP] = useState(false);
-  
+
   console.log("app running");
   const onClickHandler = () => {
     setShowP((prev) => {
@@ -19,7 +20,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showP && <p>In am new</p>}
+      <DemoOutput show={showP} />
       <Button onClick={onClickHandler}>Toggle</Button>
     </div>
   );
---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,5 @@
+const DemoOutput = (props) => {
+  return <p>{props.show ? "this is new!" : ""}</p>;
+};
+
+export default DemoOutput;
