30ddd0d19d3c18706fe475c23178735e6b77afc6
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-29 00:34:05 -0400

    adding instructors version to post data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  //Functions are obj and the below format of function will change each time a state changes
-  // function fetchmoviesHandler(){}
-  //Therefore the useEffect will reRender the fetchmoviesHandler each time a state change
-  //To prevent that it's better to use useCallback hook to prevent it comparing the changes unnecessary
-  //Below function does not have any external dependencies, fetch is a JS function, and all setStates do not needed
-  //to be added as react manages them by itself
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
-        throw new Error(response.status + " something went wrong");
+        throw new Error('Something went wrong!');
       }
+
       const data = await response.json();
 
-      const transformMovies = data.results.map((movieData) => {
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
-          releaseDate: movieData.release_date,
           openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
         };
       });
-      setMovies(transformMovies);
+      setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -43,16 +39,29 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  let content = <p>Movies not found</p>;
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
-  } else if (error) {
+  }
+
+  if (error) {
     content = <p>{error}</p>;
-  } else if (isLoading) {
+  }
+
+  if (isLoading) {
     content = <p>Loading...</p>;
   }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
