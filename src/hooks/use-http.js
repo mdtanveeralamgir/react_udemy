683ec5569352b96fd0237b50c63dede81c94d053
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-29 21:42:10 -0400

    using useHttp to fetch data from firebase db, yet to finish useEffect dependencies

---
+++ src/hooks/use-http.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      //   const loadedTasks = [];
+
+      //   for (const taskKey in data) {
+      //     loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      //   }
+
+      //   setTasks(loadedTasks);
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequest: sendRequest,
+  };
+};
+
+export default useHttp;
