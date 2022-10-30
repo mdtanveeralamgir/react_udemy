a35196b76589db75069ba59c74e1fef155268433
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-30 04:08:50 -0400

    useState and useRef to gather input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,39 @@
+import { useState, useRef } from "react";
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [nameError, setNameError] = useState(false);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value.trim());
+  };
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim().length < 1) {
+      setNameError(true);
+    } else if (nameInputRef.current.value.trim().length < 3) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+    console.log(enteredName);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          onChange={nameInputChangeHandler}
+          type="text"
+          id="name"
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
+      </div>
+      <div>
+        {nameError && <p>Please enter a name, with at least 4 char!</p>}
       </div>
     </form>
   );
