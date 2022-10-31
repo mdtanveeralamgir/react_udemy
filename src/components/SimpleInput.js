42c0a50640f5dfd51c116abc8db4859e9bf5a585
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-10-31 12:46:25 -0400

    assignment, adding another input email

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,20 +3,21 @@ const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
   const [nameError, setNameError] = useState(false);
+  const [enterEmail, setEnterEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
   const [formIsValid, setFormIsValid] = useState(false); //Form is invalid if any input is invalid
-
 
   //Using useEffect to mark form is invalid if any input is invalid
   useEffect(() => {
-
-    if (!nameError) {
+    if (!nameError && !emailError) {
       setFormIsValid(true);
     } else {
       setFormIsValid(false);
     }
-  }, [nameError]);
+  }, [nameError, emailError]);
 
-  const validateEnteredName = (name = enteredName.trim()) => {
+  const validateEnteredName = (event = null) => {
+    const name = event ? event.target.value.trim() : enteredName.trim();
     if (name.length < 1) {
       setNameError(true);
     } else if (nameInputRef.current.value.trim().length < 3) {
@@ -24,17 +25,38 @@ const SimpleInput = (props) => {
     } else {
       setNameError(false);
     }
-
   };
+
+  const validateEnteredEmail = (event = null) => {
+    const email = event ? event.target.value.trim() : enterEmail.trim();
+    const emailIncludesAt = email.toString().includes("@");
+    console.log(emailIncludesAt);
+    console.log(email);
+    if (email.length < 1 || !emailIncludesAt) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value.trim());
-    validateEnteredName(event.target.value.trim()); //Because of the react scheduling the value from DOM should be passed here
+    validateEnteredName(event); //Because of the react scheduling the value from DOM should be passed here
+  };
+
+  const emailInputChangeHandler = (event) => {
+    setEnterEmail(event.target.value.trim());
+    validateEnteredEmail(event);
   };
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     validateEnteredName();
+    validateEnteredEmail();
   };
   const nameInputClasses = nameError ? "form-control invalid" : "form-control";
+  const emailInputClasses = emailError
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -46,6 +68,15 @@ const SimpleInput = (props) => {
           onBlur={validateEnteredName}
           type="text"
           id="name"
+        />
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          onChange={emailInputChangeHandler}
+          onBlur={validateEnteredEmail}
+          type="email"
+          id="email"
         />
       </div>
       <div className="form-actions">
