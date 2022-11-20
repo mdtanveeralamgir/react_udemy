5a6307dfe71c72694b7b484ab4527b509de507b8
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-11-19 23:01:28 -0500

    incrementing and decrementing counter with button click

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,18 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
 //
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-
+  const dispatch = useDispatch();
   //While using useSelector the react will automatically create a subscription for this component
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -13,6 +20,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
