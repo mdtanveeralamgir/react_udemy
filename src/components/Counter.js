af771ac996059630bc679f787989c80c989b1c0f
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-11-19 22:56:05 -0500

    using useSelector hook to get access of the counter object from store to Counter.js and display the counter value on screen

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
 import classes from './Counter.module.css';
+//
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
+
+  //While using useSelector the react will automatically create a subscription for this component
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
