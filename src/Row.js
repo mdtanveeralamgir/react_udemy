2c88fd2c1eeb6f16a0aa01c6fe6e18ddd84813aa
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-11-21 21:58:15 -0500

    Finished

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,31 @@
 import Tile from "./Tile";
 import classes from "./Row.module.css";
 
+
 const Row = (props) => {
+  const charOfGussedWord = [...props.guess];
+  const wordChar = [...props.word.toLowerCase()];
+
   let tileRow = [];
-  for (let i = 0; i < props.wordLength; i++) {
-    tileRow.push(<Tile key={i} />);
+  for (let i = 0; i < props.word.length; i++) {
+    let tClass = "";
+    if (wordChar[i] === charOfGussedWord[i]) {
+      tClass = "rightPlace";
+    } else if (wordChar.includes(charOfGussedWord[i])) {
+      tClass = "close";
+    } else {
+      tClass = "wrong";
+    }
+    if (charOfGussedWord[i] !== undefined) {
+      tileRow.push(
+        <Tile tileClass={tClass} key={i} gusChar={charOfGussedWord[i]} />
+      );
+    } else {
+      tileRow.push(<Tile key={i} gusChar="" />);
+    }
   }
-  return <div className={classes.row}> {tileRow} </div>;
+
+  return <div className={classes.row}>{tileRow}</div>;
 };
 
 export default Row;
--- src/Tile.js
+++ src/Tile.js
@@ -1,7 +1,20 @@
 import classes from "./Tile.module.css";
 
-const Tile = () => {
-  return <div className={classes.tile}></div>;
+const Tile = (props) => {
+  let tClass = classes.tile + " ";
+
+  switch(props.tileClass){
+    case "rightPlace":
+      tClass = tClass + classes.rightPlace;
+      break;
+    case "close":
+      tClass = tClass + classes.close;
+      break;
+    case "wrong":
+      tClass = tClass + classes.wrong;
+      break;
+  }
+  return <div className={tClass}>{props.gusChar}</div>;
 };
 
 export default Tile;
