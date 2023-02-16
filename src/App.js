2606e5987657f7097f431699a38e2e51528fb546
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2023-02-16 11:31:18 -0500

    Initial set up of react hooks from section 28

--- src/App.js
+++ src/App.js
@@ -1,72 +1,9 @@
-import { useEffect, useState } from "react";
-import { WORDS } from "./WORDS";
-import Row from "./Row";
-import classes from "./main.module.css";
-function App() {
-  const [word, setWord] = useState("");
-  // const [curGuessNum, setCurGuessNum] = useState(0);
-  const [guesses, setGusses] = useState(Array(6).fill(null));
-  const [currentGuess, setCurrentGuess] = useState("");
+import React from 'react';
 
-  useEffect(() => {
-    setWord(WORDS[Math.floor(Math.random() * WORDS.length)]);
-  }, []);
+import Ingredients from './components/Ingredients/Ingredients';
 
-  useEffect(() => {
-    const typeHandler = (event) => {
-      if (event.key === "Backspace") {
-        setCurrentGuess((prev) => prev.slice(0, -1));
-      } else if (event.key === "Enter") {
-        if (currentGuess.length !== word.length) {
-          return;
-        }
-
-        if (word === currentGuess) {
-          alert("Bingo");
-          return;
-        }
-        setGusses((prev) => {
-          let temp = [...prev];
-          const nullIndex = temp.findIndex((val) => val == null);
-          temp[nullIndex] = currentGuess;
-          setCurrentGuess("");
-          return temp;
-        });
-      } else {
-        setCurrentGuess((prev) => {
-          let temp = prev;
-
-          if (temp.length < word.length) {
-            temp = temp + event.key;
-          }
-          return temp;
-        });
-      }
-
-      // if(currentGuess.length >= 5){
-      //   return;
-      // }
-    };
-
-    window.addEventListener("keydown", typeHandler);
-
-    return () => window.removeEventListener("keydown", typeHandler);
-  }, [currentGuess, word]);
-
-  return (
-    <div className={classes.board}>
-      {guesses.map((guess, i) => {
-        const isCurrentGuess = i === guesses.findIndex((val) => val == null);
-        return (
-          <Row
-            key={i}
-            word={word}
-            guess={isCurrentGuess ? currentGuess : guess ?? ""}
-          />
-        );
-      })}
-    </div>
-  );
-}
+const App = props => {
+  return <Ingredients />;
+};
 
 export default App;
