5aa02707cd511ee4ee4642b1c67f001f431039fa
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2023-02-20 23:34:51 -0500

    useReducer instead of useState

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,26 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useReducer, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((prev) => prev.id !== action.id);
+    default:
+      throw new Error("Should not get there!");
+  }
+};
+
 function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
   //Since search calls the API and returns all fruits so we do not need below call
@@ -27,7 +41,8 @@ function Ingredients() {
   // }, []);
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
@@ -42,36 +57,43 @@ function Ingredients() {
         return response.json();
       })
       .then((responseData) => {
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: responseData.name, ...ingredient },
-        ]);
+        // setUserIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient },
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: responseData.name, ...ingredient },
+        });
       });
   };
   const removeIngredientHandler = (ingredientId) => {
     setIsLoading(true);
     fetch(
-      `https://react-http-9c6c6-default-rtdb.firebaseio.com/fruits/${ingredientId}.jon`,
+      `https://react-http-9c6c6-default-rtdb.firebaseio.com/fruits/${ingredientId}.json`,
       {
         method: "DELETE",
       }
-    ).then((response) => {
-      setIsLoading(false);
-      setUserIngredients((prev) =>
-        prev.filter((ingredient) => ingredient.id !== ingredientId)
-      );
-    }).catch(error => {
-      setError(error.message);
-      setIsLoading(false);
-    });
+    )
+      .then((response) => {
+        setIsLoading(false);
+        // setUserIngredients((prev) =>
+        //   prev.filter((ingredient) => ingredient.id !== ingredientId)
+        // );
+        dispatch({ type: "DELETE", id: ingredientId });
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   };
 
   const clearError = () => {
     setError(null);
-  }
+  };
   return (
     <div className="App">
-    {error &&  <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading}
